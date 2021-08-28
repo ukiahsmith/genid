6eb0b6e50b4a3d75e012fa911686dd14a7b9954d
Ukiah Smith <ukiah@faction42.com>
2021-08-28 06:53:39 -0400

    Update to read flags and act on -n noNewLine

--- cmd/genid/main.go
+++ cmd/genid/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 
 	"github.com/gofrs/uuid"
+	"github.com/peterbourgon/ff/v3"
 )
 
 func main() {
@@ -15,12 +17,26 @@ func main() {
 }
 
 func run(args []string) error {
+	fs := flag.NewFlagSet("genid", flag.ExitOnError)
+	var (
+		noNewline = fs.Bool("n", false, "Do not print the trailing newline character.")
+	)
+
+	ff.Parse(fs, args,
+		ff.WithEnvVarPrefix("GENID"),
+	)
+
+	var lineEnding string
+	if *noNewline == false {
+		lineEnding = "\n"
+	}
+
 	id, err := uuid.NewV4()
 	if err != nil {
 		return err
 	}
 
-	fmt.Println(id)
+	fmt.Printf("%s%s", id, lineEnding)
 
 	return nil
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/ukiahsmith/genid
 
 go 1.16
 
-require github.com/gofrs/uuid v4.0.0+incompatible
+require (
+	github.com/gofrs/uuid v4.0.0+incompatible
+	github.com/peterbourgon/ff/v3 v3.1.0 // indirect
+)
